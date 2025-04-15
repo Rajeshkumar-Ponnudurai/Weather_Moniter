87b8e82e3957aacb9702a9f17c944a1687a6e140
Rajeshkumar <rajeshkumarponnudurai@gmail.com>
2025-04-15 06:42:39 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import WeatherApp from "./WeatherApp";
 
-function App(){
-    return(
-        <Router>
-            <Routes>
-                <Route path="/" element = {<Login/>} />
-                <Route path="/weather" element = {<WeatherApp/>} />
-            </Routes>
-        </Router>
-    );
+function App() {
+  return (
+    <Router basename="/Weather_Moniter">
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/weather" element={<WeatherApp />} />
+      </Routes>
+    </Router>
+  );
 }
 export default App;
